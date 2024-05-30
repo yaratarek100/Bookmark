1001693afb0dc2ebda77133418820523ba375338
yaratarek100 <yara.tarek.300@gmail.com>
2024-05-30 21:26:45 +0200

    done

--- js/script.js
+++ js/script.js
@@ -8,12 +8,13 @@ var tbody = document.querySelector("tbody");
 var urlList = [];
 
 if (localStorage.getItem("urlList")) {
-  urlList = JSON.parse(localStorage.getItem("urlList"));
-  display(urlList);
+  LocalList = JSON.parse(localStorage.getItem("urlList"));
+  display(LocalList); 
 }
 
 function validation(element) {
-  var regix = { siteName: /^[a-zA-Z0-9]{3,7}$/, siteUrl: /^[a-zA-Z0-9]{1,3}$/ };
+  var regix = { siteName: /^.{3,}$/,
+   siteUrl: /^.+\.com/ };
 
   if (regix[element.id].test(element.value)) {
     element.classList.add("is-valid");
@@ -27,9 +28,14 @@ function validation(element) {
 
 
 function deleteElement(deletedIndex) {
-  urlList.pop(deletedIndex);
-  localStorage.setItem("urlList", JSON.stringify(urlList));
-  display(urlList);
+
+   urlList.splice(deletedIndex,1);
+   localStorage.setItem("urlList", JSON.stringify(urlList));
+   LocalList= urlList;
+   urlList=[];
+   tbody.innerHTML="";
+  display(LocalList);
+
 }
 
 submit.addEventListener("click", (e) => {
@@ -39,12 +45,12 @@ submit.addEventListener("click", (e) => {
     siteName.classList.contains("is-valid") &&
     siteUrl.classList.contains("is-valid")
   ) {
+ 
     var newSite = {
       sName: siteName.value,
       sUrl: siteUrl.value,
     };
     
-    urlList.push(newSite);
     creatElement(newSite);
     localStorage.setItem("urlList", JSON.stringify(urlList));
     siteName.classList.remove("is-valid");
@@ -62,27 +68,31 @@ closeBtn.addEventListener("click", () => {
 
 
 function creatElement(newSite) {
-    var newRow = document.createElement("tr");
-    newRow.innerHTML = `<td>${newSite["sName"]}</td>
-    <td>${newSite.sName}</td>
+
+  var newRow = document.createElement("tr");
+  newRow.innerHTML = `<tr>
+  <td>${urlList.length+1}</td>
+  <td>${newSite.sName}</td>
   <td>
-  <a class="btn row-b visit-b" href="https://${newSite.sUrl}" target="_blank" >
-      <i class="fa-solid fa-eye pe-2"></i> Visit
-    </a>
-    </td>
+  <a class="btn row-b visit-b" href="${newSite.sUrl}" target="_blank" >
+  <i class="fa-solid fa-eye pe-2"></i> Visit
+  </a>
+  </td>
     <td>
     <button class="btn row-b del-b" onclick="deleteElement(${urlList.length})">
     <i class="fa-solid fa-trash-can"></i> Delete
     </button>
     </td>
+    </tr>
     `;
     tbody.appendChild(newRow);
+    urlList.push(newSite);
   }
 
 function display(list) {
-  for (i = 1; 1 < list.length; i++) {
-    // creatElement(list[i]);
-console.log(list[i]);
+
+  for (i =0; i<(list.length); i++) {
+
+    creatElement(list[i]);
   }
 }
-// localStorage.clear();
